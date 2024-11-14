fec25cf8b2ca9c7f0dfbb3681ee3c616a7cb6eed
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-11-14 09:09:41 +0530

    commit

---
+++ hooks/useScrollPosition.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const useScrollPosition = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return scrollPosition;
+};
+
+export default useScrollPosition;
