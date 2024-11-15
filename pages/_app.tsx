97024b973963ff032f9aa75c662be80b887ffc1d
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-11-15 14:49:44 +0530

    removed scroll bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,6 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div >
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </div>
 }
