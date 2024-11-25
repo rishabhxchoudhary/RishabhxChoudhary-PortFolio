5e70037db98833e074009f5714125aa1fa552a48
Rishabh Kumar Choudhary <97455703+rk26072003@users.noreply.github.com>
2024-11-25 11:25:51 +0530

    mathjax re render fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import "@/styles/globals.css";
 import "@/styles/blog.css";
 import type { AppProps } from "next/app";
 import { MathJaxContext } from 'better-react-mathjax';
-
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 // import { Inter } from '@next/font/google';
 
 // const inter = Inter({
@@ -37,6 +38,25 @@ export default function App({ Component, pageProps }: AppProps) {
       fontCache: 'global',
     },
   };
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      if (window.MathJax && window.MathJax.typeset) {
+        window.MathJax.typeset();
+      }
+    };
+
+    // Listen for route changes
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  
   return <div >
     <MathJaxContext config={mathJaxConfig}>
       <Component {...pageProps} />
