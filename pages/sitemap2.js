f77cecb0c76508c57ab2de76721101b036d98abd
support-cv <support@contravault.com>
2025-03-23 09:39:33 +0530

    added sitemap

---
+++ pages/sitemap2.js
@@ -0,0 +1,34 @@
+import { getSortedPostsData } from '../lib/posts';
+
+export default function Sitemap() {
+  return null;
+}
+
+export async function getServerSideProps({ res }) {
+    const BASE_URL = "https://www.rishabhxchoudhary.com";
+    let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+    const allPosts = getSortedPostsData();
+    console.log("allPosts", allPosts);
+
+    allPosts.map((p)=>{
+      sitemap += `
+      <url>
+         <loc>${BASE_URL + `/blog/${p.slug}`}</loc>
+         <title>${p.title}</title>
+        <changefreq>weekly</changefreq>
+        <priority>1</priority>
+      </url>
+      `
+    })
+    sitemap += `</urlset>`
+  
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(sitemap);
+    res.end();
+
+    return {
+      props: {},
+    };
+  }
